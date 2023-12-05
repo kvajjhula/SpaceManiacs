972ed7e12b05fa5720ac55539f967b1abcfb0b2b
kvajjhula <kriti.vajjhula@gmail.com>
2023-12-05 15:30:38 -0800

    preliminary ui layout astronaut

--- app/src/main/java/edu/uw/ischool/avajjh/spacemaniacs/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/avajjh/spacemaniacs/MainActivity.kt
@@ -26,6 +26,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         cardViewAstronauts.setOnClickListener {
+            val intent = Intent(this, RandomAstronautGenerator::class.java)
             Log.i("Main", "clicked astronauts")
         }
 
