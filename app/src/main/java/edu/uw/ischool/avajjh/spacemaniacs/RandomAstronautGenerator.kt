3980024bf33f2e3c04aa3882b626b5a362c0cfba
kvajjhula <kriti.vajjhula@gmail.com>
2023-12-13 15:55:15 -0800

    get some data displaying on astronaut page

--- app/src/main/java/edu/uw/ischool/avajjh/spacemaniacs/RandomAstronautGenerator.kt
+++ app/src/main/java/edu/uw/ischool/avajjh/spacemaniacs/RandomAstronautGenerator.kt
@@ -6,22 +6,37 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.TextView
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import org.w3c.dom.Text
 
 class RandomAstronautGenerator : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_random_astronaut_generator)
-        val randomizeButton = findViewById<Button>(R.id.randomizeButton);
+        val randomizeButton = findViewById<Button>(R.id.randomizeButton)
+        val astronautName = findViewById<TextView>(R.id.astronautName)
+        val astronautAge = findViewById<TextView>(R.id.astronautAge)
+        val astronautNationality = findViewById<TextView>(R.id.astronautNationality)
+        val astronautFlight = findViewById<TextView>(R.id.astronautFlightCount)
+        val astronautBio = findViewById<TextView>(R.id.astronautBio)
+
         randomizeButton.setOnClickListener() {
             GlobalScope.launch(Dispatchers.Main) {
                 fetchAndProcessData()
                 Log.i("button", "clicked on randomize")
                 (application as RepositoryApplication).update("astronaut")
                 val astronautArray: Array<Astronaut> = (application as RepositoryApplication).repository.getAstronauts()
-               
+                val astronautArrayLength = astronautArray.size - 1 // should be 10?
+                val randomAstronaut = astronautArray[(0..astronautArrayLength).random()]
+
+                astronautName.text = randomAstronaut.name
+                astronautAge.text = "Age:  ${randomAstronaut.age}"
+                astronautNationality.text = randomAstronaut.nationality
+                astronautFlight.text = "Flight Count: ${randomAstronaut.flightCount}"
+                astronautBio.text = "Bio: ${randomAstronaut.bio}"
             }
         }
     }
